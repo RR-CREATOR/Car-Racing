5d6693419765fe705b6036b184ee50194d573234
RR-CREATOR <77535915+RR-CREATOR@users.noreply.github.com>
2021-03-16 18:23:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -83,4 +83,92 @@ function my_keydown(e){
         car2_down();
         console.log("key s");
     }
+    if(car1_x > 700){
+        console.log("car1 Won");
+        document.getElementById('game_status').innerHTML = "Car 1 Won!!";
+    }
+    if(car2_x > 700){
+        console.log("car2 Won");
+        document.getElementById('game_status').innerHTML = "Car 2 Won!!";
+    }
+}
+
+function car1_up()
+{
+    if(car1_y >= 0){
+        car1_y = car1_y - 10;
+        console.log("When up arrow pressed, " + "x = " + car1_x + " | y = " + car1_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_up()
+{
+    if(car2_y >= 0){
+        car2_y = car2_y - 10;
+        console.log("When w key pressed, " + "x = " + car2_x + " | y = " + car2_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_down()
+{
+    if(car1_y <= 500){
+        car1_y = car1_y + 10;
+        console.log("When down arrow pressed, " + "x = " + car1_x + " | y = " + car1_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_down()
+{
+    if(car2_y <= 500){
+        car2_y = car2_y + 10;
+        console.log("When s key pressed, " + "x = " + car2_x + " | y = " + car2_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_left()
+{
+    if(car1_x >= 0){
+        car1_x = car1_x - 10;
+        console.log("When left arrow pressed, " + "x = " + car1_x + " | y = " + car1_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_left()
+{
+    if(car2_x >= 0){
+        car2_x = car2_x - 10;
+        console.log("When a key pressed, " + "x = " + car2_x + " | y = " + car2_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_right()
+{
+    if(car1_x <= 500){
+        car1_x = car1_x + 10;
+        console.log("When right arrow pressed, " + "x = " + car1_x + " | y = " + car1_y);
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_right()
+{
+    if(car2_x <= 500){
+        car2_x = car2_x + 10;
+        console.log("When right arrow pressed, " + "x = " + car2_x + " | y = " + car2_y);
+        uploadcar1();
+        uploadcar2();
+    }
 }
